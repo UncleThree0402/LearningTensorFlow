679e3b370b2091dbff6f1e0f01cf4b2a7c6e238e
UncleThree0402 <wonghokchi0402@gmail.com>
2021-11-27 22:41:18 +0800

    Update Manipulation Tensor
    Dot Product and Transpose

--- Fundamentals/ManipulatingTensor/README.md
+++ Fundamentals/ManipulatingTensor/README.md
@@ -2,3 +2,7 @@
 <pre>
 Using tf method will be faster while using gpu to cal
 </pre>
+
+<pre>
+Dot product with @ or tf.matmul() or tf.tensordot()
+Transpose with tf.transpose()</pre>
--- Fundamentals/ManipulatingTensor/manipulation.py
+++ Fundamentals/ManipulatingTensor/manipulation.py
@@ -18,3 +18,28 @@ print(tensorA * 10)
 print(tf.multiply(tensorA, 1.5))
 
 print(tf.divide(tensorA, 0.5))
+
+# matrix manipulation
+
+randomOne = tf.random.Generator.from_seed(100)
+randomOne = randomOne.uniform(shape=(2, 3))
+
+print(randomOne)
+
+randomTwo = tf.random.Generator.from_seed(50)
+randomTwo = randomTwo.uniform(shape=(3, 2))
+print(randomTwo)
+
+print(randomOne @ randomTwo)
+print(tf.matmul(randomOne, randomTwo))
+print(tf.tensordot(randomOne, randomTwo))
+# reshape
+
+print(randomOne @ tf.reshape(randomOne, shape=(3, 2)))
+
+# transpose
+
+print(tf.transpose(randomOne))
+
+
+
