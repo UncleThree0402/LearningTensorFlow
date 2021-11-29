84cdc44ebec0d9715240f2266f3b36c335a11c60
UncleThree0402 <wonghokchi0402@gmail.com>
2021-11-30 00:59:36 +0800

    Update ManipulatingTensor
    Square, Square Root, Log

--- Fundamentals/ManipulatingTensor/README.md
+++ Fundamentals/ManipulatingTensor/README.md
@@ -6,3 +6,6 @@ Using tf method will be faster while using gpu to cal
 <pre>
 Dot product with @ or tf.matmul() or tf.tensordot()
 Transpose with tf.transpose()</pre>
+
+<pre>
+tf.math for math</pre>
--- Fundamentals/ManipulatingTensor/manipulation.py
+++ Fundamentals/ManipulatingTensor/manipulation.py
@@ -2,44 +2,53 @@ import tensorflow as tf
 
 tensorA = tf.ones(shape=(4, 4))
 
-print(tensorA)
+print(tensorA, "\n")
 
 # Addition
-print(tensorA + 10)
+print(tensorA + 10, "\n")
 
 # Subtraction
-print(tensorA - 10)
+print(tensorA - 10, "\n")
 
 # Multiplication
-print(tensorA * 10)
+print(tensorA * 10, "\n")
 
 # using tensorflow method
 
-print(tf.multiply(tensorA, 1.5))
+print(tf.multiply(tensorA, 1.5), "\n")
 
-print(tf.divide(tensorA, 0.5))
+print(tf.divide(tensorA, 0.5), "\n")
+
+# square
+tensorA = tf.range(1, 10)
+print(tensorA, "\n")
+
+print(tf.math.square(tensorA), "\n")
+
+# sqrt
+print(tf.math.sqrt(tf.cast(tensorA, dtype=tf.float32)), "\n")
+
+# log
+print(tf.math.log(tf.cast(tensorA, dtype=tf.float32)), "\n")
 
 # matrix manipulation
 
 randomOne = tf.random.Generator.from_seed(100)
 randomOne = randomOne.uniform(shape=(2, 3))
 
-print(randomOne)
+print(randomOne, "\n")
 
 randomTwo = tf.random.Generator.from_seed(50)
 randomTwo = randomTwo.uniform(shape=(3, 2))
-print(randomTwo)
+print(randomTwo, "\n")
 
-print(randomOne @ randomTwo)
-print(tf.matmul(randomOne, randomTwo))
-print(tf.tensordot(randomOne, randomTwo))
+print(randomOne @ randomTwo, "\n")
+print(tf.matmul(randomOne, randomTwo), "\n")
+print(tf.tensordot(randomOne, randomTwo, axes=0), "\n")
 # reshape
 
-print(randomOne @ tf.reshape(randomOne, shape=(3, 2)))
+print(randomOne @ tf.reshape(randomOne, shape=(3, 2)), "\n")
 
 # transpose
 
-print(tf.transpose(randomOne))
-
-
-
+print(tf.transpose(randomOne), "\n")
