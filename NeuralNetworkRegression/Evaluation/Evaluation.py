2559335e6b46fe6c8dc134214df678639da3b791
marcowong <wonghokchi0402@gmail.com>
2021-12-02 17:16:18 +0800

    Update Evaluation
    Visualise, Three Set, Parameter, Plot_model and Error

--- NeuralNetworkRegression/Evaluation/Evaluation.py
+++ NeuralNetworkRegression/Evaluation/Evaluation.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from tensorflow.keras.utils import plot_model
 
 X = tf.range(0, 100)
 y = tf.add(X, 32)
-plt.scatter(X, y)
+# plt.scatter(X, y)
 # plt.show()
 
 # Split Train and Test
@@ -15,17 +16,19 @@ y_train = y[:80]
 y_test = y[80:]
 
 # Visualise
-plt.figure(figsize=(10, 7))
+plt.figure(figsize=(20, 14))
 plt.scatter(X_train, y_train, c="b", label="Training Data")
 plt.scatter(X_test, y_test, c="g", label="Testing Data")
 plt.legend()
 plt.show()
 
+tf.random.set_seed(42)
+
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(1),
+    tf.keras.layers.Dense(1, input_shape=[1], name="input_layer"),
     tf.keras.layers.Dense(4, activation="relu"),
-    tf.keras.layers.Dense(1)
-])
+    tf.keras.layers.Dense(1, name="output_layer"),
+], name="ModelOne")
 
 model.compile(loss=tf.keras.losses.mae,
               optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
@@ -34,3 +37,37 @@ model.compile(loss=tf.keras.losses.mae,
 model.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs=500)
 
 model.summary()
+
+plot_model(model=model, show_shapes=True)
+
+y_pred = model.predict(X_test)
+
+print(y_test, "\n")
+print(tf.squeeze(y_pred), "\n")
+
+
+def predict_function(training_data=X_train,
+                     training_labels=y_train,
+                     test_data=X_test,
+                     test_labels=y_test,
+                     pred=y_pred):
+    plt.figure(figsize=(20, 14))
+    plt.scatter(training_data, training_labels, c="b", label="Training Data")
+    plt.scatter(test_data, test_labels, c="g", label="Test Data")
+    plt.scatter(test_data, pred, c="r", label="Prediction")
+    plt.legend()
+    plt.show()
+
+
+predict_function()
+
+# Evaluate the test data
+
+model.evaluate(X_test, y_test)
+
+# mae
+print(tf.metrics.mean_absolute_error(y_test, tf.squeeze(y_pred)), "\n")
+
+# mse
+print(tf.metrics.mean_squared_error(y_test, tf.squeeze(y_pred)), "\n")
+
--- NeuralNetworkRegression/Evaluation/README.md
+++ NeuralNetworkRegression/Evaluation/README.md
@@ -10,3 +10,18 @@ Training Set - model learn from 70-80 %
 Validation Set - model get tuned one 10-15 %
 Test Set - model get evaluated from 10-15 %
 </pre>
+
+### Parameter
+<pre>
+Total parameter : total number of parameter of model
+Trainable parameter : Parameter(s) (patterns) which can update
+Non-trainable parameter : Parameter that can't update (Already trained)</pre>
+
+### Plot_Model
+<pre>
+plot_model(model=model)</pre>
+
+### Error
+<pre>
+mae : tf.metrics.mean_absolute_error(y_true, y_pred)
+mse : tf.metrics.mean_squared_error(y_true, y_pred)</pre>
