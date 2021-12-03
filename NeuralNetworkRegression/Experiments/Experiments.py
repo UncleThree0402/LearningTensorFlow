6a2b64476882c348a5a5a6c2d677cd0bb1295419
UncleThree0402 <wonghokchi0402@gmail.com>
2021-12-03 12:14:47 +0800

    Update Experiments

--- NeuralNetworkRegression/Experiments/Experiments.py
+++ NeuralNetworkRegression/Experiments/Experiments.py
@@ -2,6 +2,10 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.utils import plot_model
+import pandas as pd
+import os
+
+os.environ["PATH"] += os.pathsep + 'C:/Program Files/Graphviz/bin/'
 
 X = tf.range(-10, 10, dtype=tf.int32)
 y = tf.subtract(X, 24)
@@ -15,22 +19,24 @@ y_test = y[16:]
 tf.random.set_seed(42)
 
 
-def plot_pred(training_data=X_train,
+def plot_pred(prediction,
+              title,
+              training_data=X_train,
               training_label=y_train,
               testing_data=X_test,
-              testing_label=y_test,
-              *prediction):
+              testing_label=y_test,):
     plt.figure(figsize=(10, 7))
     plt.scatter(training_data, training_label, c="b", label="Training")
     plt.scatter(testing_data, testing_label, c="g", label="Testing")
     plt.scatter(testing_data, prediction, c="r", label="Prediction")
     plt.legend()
+    plt.title(title)
     plt.show()
 
 
 # First Model
 modelOne = tf.keras.Sequential([
-    tf.keras.layers.Dense(1, input_shape=[1])
+    tf.keras.layers.Dense(1)
 ], name="ModelOne")
 
 modelOne.compile(loss=tf.keras.losses.mae,
@@ -41,13 +47,15 @@ modelOne.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs=100)
 
 modelOnePred = tf.squeeze(modelOne.predict(X_test))
 
-plot_pred(X_train, y_train, X_test, y_test, modelOnePred)
+plot_pred(prediction=tf.squeeze(modelOnePred), title=modelOne.name)
+
+plot_model(model=modelOne, to_file="model_one.png", show_shapes=True)
 
 print("\n")
 
 # Second Model
 modelTwo = tf.keras.Sequential([
-    tf.keras.layers.Dense(1, input_shape=[1], name="input_layer"),
+    tf.keras.layers.Dense(1),
     tf.keras.layers.Dense(1)
 ], name="ModelTwo")
 
@@ -55,17 +63,19 @@ modelTwo.compile(loss=tf.keras.losses.mae,
                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
                  metrics=["mae"])
 
-modelTwo.fit(X_train, y_train, epochs=100)
+modelTwo.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs=100)
 
 modelTwoPred = tf.squeeze(modelTwo.predict(X_test))
 
-plot_pred(X_train, y_train, X_test, y_test, modelTwoPred)
+plot_pred(prediction=modelTwoPred, title=modelTwo.name)
+
+plot_model(model=modelTwo, to_file="model_two.png", show_shapes=True)
 
 print("\n")
 
 # Third Model
 modelThree = tf.keras.Sequential([
-    tf.keras.layers.Dense(1, input_shape=[1], name="input_layer"),
+    tf.keras.layers.Dense(1),
     tf.keras.layers.Dense(1)
 ], name="ModelThree")
 
@@ -73,19 +83,32 @@ modelThree.compile(loss=tf.keras.losses.mae,
                    optimizer=tf.keras.optimizers.Adam(learning_rate=0.01),
                    metrics=["mae"])
 
-modelThree.fit(X_train, y_train, epochs=500)
+modelThree.fit(tf.expand_dims(X_train, axis=-1), y_train, epochs=500)
 
 modelThreePred = tf.squeeze(modelThree.predict(X_test))
 
-plot_pred(X_train, y_train, X_test, y_test, modelThreePred)
+plot_pred(prediction=modelThreePred, title=modelThree.name)
+
+plot_model(model=modelThree, to_file="model_three.png", show_shapes=True)
 
 print("\n")
 
-print("mae : ", tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelOnePred)).numpy())
-print("mse : ", tf.metrics.mean_squared_error(y_test, tf.squeeze(modelOnePred)).numpy(), "\n")
+modelOneMae = tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelOnePred)).numpy()
+modelOneMse = tf.metrics.mean_squared_error(y_test, tf.squeeze(modelOnePred)).numpy()
 
-print("mae : ", tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelTwoPred)).numpy())
-print("mse : ", tf.metrics.mean_squared_error(y_test, tf.squeeze(modelTwoPred)).numpy(), "\n")
+modelTwoMae = tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelTwoPred)).numpy()
+modelTwoMse = tf.metrics.mean_squared_error(y_test, tf.squeeze(modelTwoPred)).numpy()
 
-print("mae : ", tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelThreePred)).numpy())
-print("mse : ", tf.metrics.mean_squared_error(y_test, tf.squeeze(modelThreePred)).numpy(), "\n")
+modelThreeMae = tf.metrics.mean_absolute_error(y_test, tf.squeeze(modelThreePred)).numpy()
+modelThreeMse = tf.metrics.mean_squared_error(y_test, tf.squeeze(modelThreePred)).numpy()
+
+model_result = [["Model One", modelOneMae, modelOneMse],
+                ["Model Two", modelTwoMae, modelTwoMse],
+                ["Model Three", modelThreeMae, modelThreeMse]]
+
+result = pd.DataFrame(model_result, columns=["Model", "mae", "mse"])
+print(result, "\n")
+
+modelOne.summary()
+modelTwo.summary()
+modelThree.summary()
---
+++ NeuralNetworkRegression/Experiments/README.md
@@ -0,0 +1,6 @@
+# Experiments
+
+<pre>
+First model : One Layer, epochs 100
+Second model : Two Layer, epochs 100
+Third model : Two Layers, epochs 200</pre>
