532c165c113d2c2c73257ce38dbdf8aec469141b
UncleThree0402 <wonghokchi0402@gmail.com>
2022-02-02 00:33:06 +0800

    ADDed image.py

---
+++ PlotGraph/image.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import random
+import os
+
+
+def view_random_image(target_dir, class_name):
+    folder = target_dir + class_name
+    random_image = random.sample(os.listdir(folder), 1)
+    print(random_image)
+    img = mpimg.imread(folder + "/" + random_image[0])
+    plt.imshow(img)
+    plt.title(class_name)
+    plt.axis("off")
+    plt.show()
+    print(img.shape)
